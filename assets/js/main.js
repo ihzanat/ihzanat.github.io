a60403262513be5e19f82c8923d31cf692781afe
ihzanat <ehzaaan@gmail.com>
2024-09-25 11:21:46 +0300

    final changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,29 +5,21 @@
 */
 
 (function($) {
-
-	var csrftoken = getCookie('csrftoken');
-	console.log("csrftoken = ", csrftoken)
 	var	$window = $(window),
 		$body = $('body'),
 		$nav = $('#nav');
 
 	$("#contactForm").validator().on("submit", function (event) {
 		$("#msgSubmit").hide();
-		if(csrftoken){
-			if (event.isDefaultPrevented()) {
-				// handle the invalid form...
-				$("#msgSubmit").text("Did you fill in the form properly?");
-				$("#msgSubmit").show().delay(5000).fadeOut();
-			} else {
-				// everything looks good!
-				$("#msgSubmit").fadeOut();
-				event.preventDefault();
-				submitForm();
-			}
-		}else{
-			$("#msgSubmit").text("Unable to generate token!");
+		if (event.isDefaultPrevented()) {
+			// handle the invalid form...
+			$("#msgSubmit").text("Did you fill in the form properly?");
 			$("#msgSubmit").show().delay(5000).fadeOut();
+		} else {
+			// everything looks good!
+			$("#msgSubmit").fadeOut();
+			event.preventDefault();
+			submitForm();
 		}
 		
 	});
@@ -58,13 +50,9 @@
 
 function submitForm(){
 	$("#id_contact_spinner").show();
-	console.log("csrftoken = ", csrftoken)
 	$.ajax({
 		type: "POST",
 		url: "https://webstersolution.com/wrk-contact/",
-		headers:{
-			"X-CSRFToken": csrftoken,
-		},
 		async: true,
 		data:{
 			 name: JSON.stringify($("#name").val()),
@@ -87,22 +75,4 @@ function submitForm(){
 		}
 	});
 	
-}
-
-
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-	console.log("cookieValue = ", cookieValue)
-    return cookieValue;
 }
